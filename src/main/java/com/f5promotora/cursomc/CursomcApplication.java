408c76d806da0394792a201ab6bdcccb6b7791bf
Anderson Freitas <andersonfreitas21@gmail.com>
2019-02-06 14:01:25 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/f5promotora/cursomc/CursomcApplication.java
+++ src/main/java/com/f5promotora/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.f5promotora.cursomc.domain.Cidade;
 import com.f5promotora.cursomc.domain.Cliente;
 import com.f5promotora.cursomc.domain.Endereco;
 import com.f5promotora.cursomc.domain.Estado;
+import com.f5promotora.cursomc.domain.ItemPedido;
 import com.f5promotora.cursomc.domain.Pagamento;
 import com.f5promotora.cursomc.domain.PagamentoComBoleto;
 import com.f5promotora.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.f5promotora.cursomc.repositories.CidadeRepository;
 import com.f5promotora.cursomc.repositories.ClienteRepository;
 import com.f5promotora.cursomc.repositories.EnderecoRepository;
 import com.f5promotora.cursomc.repositories.EstadoRepository;
+import com.f5promotora.cursomc.repositories.ItemPedidoRepository;
 import com.f5promotora.cursomc.repositories.PagamentoRepository;
 import com.f5promotora.cursomc.repositories.PedidoRepository;
 import com.f5promotora.cursomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner {
 	private PagamentoRepository pagamentoRepository;
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -113,6 +117,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p1.getItens().addAll(Arrays.asList(ip3));
+		p1.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
 	}
 
 }
--- src/main/java/com/f5promotora/cursomc/domain/Pedido.java
+++ src/main/java/com/f5promotora/cursomc/domain/Pedido.java
@@ -2,6 +2,9 @@ package com.f5promotora.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,8 +12,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-
 
 @Entity
 public class Pedido implements Serializable {
@@ -21,16 +24,19 @@ public class Pedido implements Serializable {
 	private Integer id;
 	private Date instante;
 
-	@OneToOne(cascade=CascadeType.ALL,mappedBy="pedido")
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
-	
+
 	@ManyToOne
-	@JoinColumn(name="cliente_id")
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 
 	@OneToOne
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+
+	@OneToMany(mappedBy  = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Pedido() {
 	}
@@ -81,6 +87,14 @@ public class Pedido implements Serializable {
 
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
