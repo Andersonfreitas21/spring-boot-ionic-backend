72485f23cbab7f3172443133beff3f8604d4d947
Anderson Freitas <andersonfreitas21@gmail.com>
2019-02-07 17:35:03 -0300

    Banco Local

--- src/main/java/com/f5promotora/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/f5promotora/cursomc/resources/CategoriaResources.java
@@ -39,4 +39,11 @@ public class CategoriaResources {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@RequestMapping(value = "/{id}",method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
